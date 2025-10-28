46fbf537e857f73e8be2f2cb7d08a34e37398abf
Aryna Koushyk <aryna.koushyk@gmail.com>
2025-10-29 01:51:04 +0300

    remake again lesson-02-02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,10 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 7);
-let dice2 = Math.floor(Math.random() * 7);
-let isWinningDouble = (dice1 === dice2)&&(dice1&&dice2>=3);
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+
+let isWinningDouble = dice1 === dice2 && dice1 > 3;
 
 console.log("Первый бросок: " + dice1);
 console.log("Второй бросок: " + dice2);
@@ -18,3 +19,4 @@ if (isWinningDouble) {
 } else {
   console.log("Не выигрышный дубль.");
 }
+
