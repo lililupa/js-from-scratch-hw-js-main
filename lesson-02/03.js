d8e95b495a906ef6a0e97a0717f9ae424aaf3620
Aryna Koushyk <aryna.koushyk@gmail.com>
2025-10-29 16:58:32 +0300

    add a random score ti lesson-02-03

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -2,23 +2,23 @@
  * Напишите код, который будет определять оценку студента (grade) в зависимости от балла (score).
  * Балл может быть целым числом от 0 до 100, а оценки 'F' (0 - 49), 'D' (50 - 69), 'C' (70-79), 'B' (80 - 89), 'A' (90 - 100)
  * Результат сохраните в переменной grade.
- */
+//  */
 
-const score = 33; // тестовое значение, можно изменять
+const score = Math.floor(Math.random() * 101); // тестовое значение, можно изменять
 let grade;
 if (score >= 0 && score < 50) {
   grade = "F";
-  console.log(grade);
+  console.log(grade, score);
 } else if (score < 70) {
   grade = "D";
-  console.log(grade);
+  console.log(grade, score);
 } else if (score < 80) {
   grade = "C";
-  console.log(grade);
+  console.log(grade, score);
 } else if (score < 90) {
   grade = "B";
-  console.log(grade);
+  console.log(grade, score);
 } else if (score <= 100) {
   grade = "A";
-  console.log(grade);
+  console.log(grade, score);
 }
